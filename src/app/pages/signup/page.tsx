6948f66cb37536a581cd9e99baa1bb71b816c530
seo myeong jin <seomyeongjin99@gmail.com>
2024-03-06 17:05:32 +0900

    <Feat : 회원가입 페이지 css>

--- src/app/pages/signup/page.tsx
+++ src/app/pages/signup/page.tsx
@@ -1,15 +1,16 @@
-import * as React from "react";
+"use client";
 
-import { Button } from "@/components/ui/button";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+
 import {
   Card,
   CardContent,
   CardDescription,
-  CardFooter,
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
-import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import {
   Select,
@@ -18,40 +19,128 @@ import {
   SelectTrigger,
   SelectValue,
 } from "@/components/ui/select";
+import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { useState } from "react";
 
-export default function CardWithForm() {
+const formSchema = z.object({
+  username: z.string().min(2, "이름은 2글자 이상이어야 합니다."),
+  email: z.string().email("올바른 이메일을 입력해주세요."),
+  password: z.string().min(6, "비밀번호는 최소 6자 이상이여야 합니다."),
+  confirmpassword: z.string().min(6, "비밀번호는 최소 6자 이상이여야합니다."),
+});
+
+function ProfileForm() {
+  const [Validate, setValidate] = useState<boolean>(false);
+
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      username: "",
+      email: "",
+      password: "",
+      confirmpassword: "",
+    },
+  });
+
+  const onSubmit = (data: any) => {
+    console.log(data);
+  };
+
   return (
-    <Card className="w-[350px]">
-      <CardHeader>
-        <CardTitle>SIGN UP</CardTitle>
-        <CardDescription>Please fill in all the blanks.</CardDescription>
-      </CardHeader>
-      <CardContent>
-        <form>
-          <div className="grid w-full items-center gap-4">
-            <div className="flex flex-col space-y-1.5">
-              <Label htmlFor="name">Name</Label>
-              <Input id="name" placeholder="홍길동" />
-            </div>
-            <div className="flex flex-col space-y-1.5">
-              <Label htmlFor="id">ID</Label>
-              <Input id="id" placeholder="ID" />
-            </div>
-            <div className="flex flex-col space-y-1.5">
-              <Label htmlFor="password">PW</Label>
-              <Input id="password" placeholder="password" />
-            </div>
-            <div className="flex flex-col space-y-1.5">
-              <Label htmlFor="confirmpassword">CONFIRM PW</Label>
-              <Input id="confirmpassword" placeholder="ID" />
-            </div>
-          </div>
-        </form>
-      </CardContent>
-      <CardFooter className="flex justify-between">
-        <Button variant="outline">CANCEL</Button>
-        <Button>NEXT</Button>
-      </CardFooter>
-    </Card>
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        height: "100vh",
+      }}
+    >
+      <Card className="w-[400px]">
+        <CardHeader>
+          <CardTitle>계정을 생성합니다</CardTitle>
+          <CardDescription>필수 정보를 입력해볼게요.</CardDescription>
+        </CardHeader>
+        <CardContent>
+          <Form {...form}>
+            <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+              <FormField
+                control={form.control}
+                name="username"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>이름</FormLabel>
+                    <FormControl>
+                      <Input placeholder="홍길동" {...field} />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name="email"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>이메일</FormLabel>
+                    <FormControl>
+                      <Input
+                        placeholder="hello@sparta-devcamp.com"
+                        {...field}
+                      />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+
+              <FormField
+                control={form.control}
+                name="password"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>비밀번호</FormLabel>
+                    <FormControl>
+                      <Input
+                        placeholder="비밀번호를 입력해주세요"
+                        type="password"
+                        {...field}
+                      />
+                    </FormControl>
+
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name="confirmpassword"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>비밀번호 확인</FormLabel>
+                    <FormControl>
+                      <Input placeholder="*****" type="password" {...field} />
+                    </FormControl>
+
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <Button type="submit">계정생성하기</Button>
+            </form>
+          </Form>
+        </CardContent>
+      </Card>
+    </div>
   );
 }
+
+export default ProfileForm;
